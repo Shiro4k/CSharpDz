74f21a8ab550575b930cc6430a32a2fa86baea83
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-08-31 21:44:23 +0800

    add Tusk 8

---
+++ Tusk_8/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите Число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int sum = 0;
+while (count < N)
+{
+    if (sum < N)
+    {
+        sum = sum + 2;
+        System.Console.WriteLine(sum);
+    }
+    count++;
+}
