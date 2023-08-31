cea491bee60a10c4c6ca59a309dd2e6f0ae996d3
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-08-31 21:25:55 +0800

    add Tusk 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+System.Console.Write("Введите Число 1 : ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите Число 2 : ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите Число 3 : ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (max < numB)
+{
+    max = numB;
+}
+if (max < numC)
+{
+    max = numC;
+}
+System.Console.Write("Максимальное -> ");
+System.Console.Write(max);
+
