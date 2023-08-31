218d3b9b29439753337ee9465471c8e1694114fe
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-08-31 21:32:51 +0800

    add Tusk 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+System.Console.Write("Введите Число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0 )
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
