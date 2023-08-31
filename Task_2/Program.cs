eb4672a1efa42e1464c784d8815f91dbe48af2e1
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-08-31 20:46:04 +0800

    add Tusk 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите Число 1 : ");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите Число 2 : ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+{
+    System.Console.Write("1 Число больше оно ровно :  ");
+    System.Console.WriteLine(numA);
+}
+else
+{
+    System.Console.Write("2 Число больше оно ровно :  ");
+    System.Console.WriteLine(numB);
+}
