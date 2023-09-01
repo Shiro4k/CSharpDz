3cd66d0c3f576d5bb8605fb28563512cb9d7c9b6
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-09-01 10:24:49 +0800

    Update Program.cs

--- Tusk_8/Program.cs
+++ Tusk_8/Program.cs
@@ -1,8 +1,7 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 System.Console.Write("Введите Число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int sum = 0;
+int count = 2;
 while (count < N)
 {
     System.Console.WriteLine(count);
